491eee93eb5bd3dceae9db9e620554761498dfba
munnaMia <mdmunnamia987@gmail.com>
2026-02-13 22:05:59 +0600

    supervar explane

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 
 <?php
 
+// var_dump(['a;lsdjkfl;']) // this mean var dump or variable dump 
+// we can't echo to web apage a array or object but we can dump
+
+
+function dumpAndDie($value)
+{
+    echo "<pre>"; // give all info about a req
+    // var_dump($_SERVER);
+    var_dump($value);
+    echo "</pre>";
+
+    die(); // kill programme or stop exicution after that
+}
+
+// dumpAndDie($_SERVER);
+echo $_SERVER['REQUEST_URI'];
+
 $headline = "Home Page";
 
 require 'views/index.view.php';
-
-
