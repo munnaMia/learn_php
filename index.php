1a938ca542e83d4bb6c639851080ff03646a47b7
munnaMia <mdmunnamia987@gmail.com>
2026-02-13 10:13:19 +0600

    Associat array and basic filtering

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Test demo</h1>
+
+    <?php
+    $books = [
+        [
+            'name' => 'dopamin diasdtox',
+            'auther' => 'munna',
+            'url' => '#',
+            'year' => 2016
+        ],
+        [
+            'name' => 'dopamin 4r23',
+            'auther' => 'munna',
+            'url' => '#',
+            'year' => 2016
+        ],
+        [
+            'name' => 'bangladesh',
+            'auther' => 'munna',
+            'url' => '#',
+            'year' => 2010
+        ],
+        [
+            'name' => '234234 ditox',
+            'auther' => 'mia',
+            'url' => '#',
+            'year' => 2016
+        ]
+    ];
+    ?>
+
+    <ul>
+        <?php foreach ($books as $book): ?>
+            <?php if ($book['auther'] === 'munna'): ?>
+                <li>
+                    <a href="<?= $book['url'] ?>">
+                        <?= $book['name'] ?> ===== <?= $book['auther'] ?>
+                    </a>
+                </li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+
+
+</body>
+
+</html>
