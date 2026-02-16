7e1f8b783d1fd2a680eb22bb9bde1ae2e61fe1da
munnaMia <mdmunnamia987@gmail.com>
2026-02-16 11:07:08 +0600

    final cofing update

--- config.php
+++ config.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'learnPHP',
-    'charset' => 'utf8mb4',
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'learnPHP',
+        'charset' => 'utf8mb4',
+    ],
 ];
